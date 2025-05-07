c038ee3823c0d1c570c124ed7c3a9be40aaa17d4
Tshelleh <165481660+Tshelleh@users.noreply.github.com>
2025-05-07 20:34:04 +0300

    Update Drug type

--- src/types/Drug.ts
+++ src/types/Drug.ts
@@ -1,5 +1,15 @@
-import { Document } from 'mongoose';
+import { Document} from 'mongoose';
 
 export interface IDrug extends Document {
+  drugId: String; //barcode
   name: string;
+  Quantity: number;
+  stripInTHeBox: number ;
+  sample: boolean
+  ExpiryDate: Date;
+  DailyConsumption?:number[];  // count of drugs consumed par day
+
+ // Timestamps
+  createdAt?: Date;
+  updatedAt?: Date;
 } 
