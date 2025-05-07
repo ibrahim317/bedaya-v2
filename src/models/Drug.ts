0e52f094a65445918f6e5c734e5944d99bd3a2f0
Tshelleh <165481660+Tshelleh@users.noreply.github.com>
2025-05-07 20:35:20 +0300

    Update Drug models

--- src/models/Drug.ts
+++ src/models/Drug.ts
@@ -1,8 +1,16 @@
-import { Schema, model, Model } from 'mongoose';
+import { Schema, model, Model, models } from 'mongoose';
 import { IDrug } from '@/types/Drug';
 
 const DrugSchema: Schema<IDrug> = new Schema({
-  name: { type: String, required: true, unique: true, index: true },
-}, { timestamps: true });
-
-export const Drug: Model<IDrug> = model<IDrug>('Drug', DrugSchema); 
+  drugId: { type: String, required: true, unique: true, index: true },
+  name: { type: String, required: true, index: true },
+  Quantity: { type: Number, required: true },
+  stripInTHeBox: { type: Number, required: true },
+  sample: { type: Boolean, required: true ,default: false },
+  ExpiryDate: { type: Date, required: true },
+  DailyConsumption: { type: [Number], required: false, default: [0, 0, 0, 0, 0] },
+  createdAt: { type: Date, required: false, default: Date.now },
+  updatedAt: { type: Date, required: false, default: Date.now },
+});
+const Drug: Model<IDrug> = models.Drug || model<IDrug>('Drug', DrugSchema);
+export default Drug; 
